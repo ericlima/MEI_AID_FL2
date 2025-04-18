6d82e0868c024cc8b9aa3c1290daaf9b962b661a
Eric Lima <eric.george.lima@gmail.com>
2025-04-18 18:42:41 +0100

    upd

--- src/main/java/pt/ipcb/ad/aid_pl2/model/Utilizador.java
+++ src/main/java/pt/ipcb/ad/aid_pl2/model/Utilizador.java
@@ -3,11 +3,9 @@ package pt.ipcb.ad.aid_pl2.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import lombok.Data;
 import org.springframework.data.annotation.Id;
 
 @Entity
-@Data
 public class Utilizador {
     @jakarta.persistence.Id
     @Id
@@ -16,4 +14,46 @@ public class Utilizador {
     private String nome;
     private String email;
     private String password;
+
+    public Utilizador(Long id, String nome, String email, String password) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Utilizador() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
