4e7a079a0e0831b7eb82df29e89b5c4bc37b509f
Eric Lima <eric.george.lima@gmail.com>
2025-04-19 20:26:01 +0100

    fl5

--- src/main/java/pt/ipcb/ad/aid_pl2/Application.java
+++ src/main/java/pt/ipcb/ad/aid_pl2/Application.java
@@ -2,8 +2,10 @@ package pt.ipcb.ad.aid_pl2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class Application {
 
     public static void main(String[] args) {
